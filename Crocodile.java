807d2338f4d4d2b94a970bf4c21b98247dba7766
AidenS588 <101139044+AidenS588@users.noreply.github.com>
2022-04-29 13:29:28 -0400

    Added Crocodile

---
+++ Crocodile.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Crocodile extends Actor
+{
+    /**
+     * Act - do whatever the Crocodile wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(-1);// Add your action code here.
+    }
+}
